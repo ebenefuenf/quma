9a5db828fb244823467b4f77a86695c30f5fcc76
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2018-02-23 23:06:52 +0100

    Add class Namespace to db module

--- quma/db.py
+++ quma/db.py
@@ -1,4 +1,48 @@
+from itertools import chain
 from pathlib import Path
+
+
+class Namespace(object):
+    def __init__(self, db, sqldir):
+        self.db = db
+        self.sqldir = sqldir
+        self.cache = db.cache
+        self.show = db.show
+        self.context_callback = None
+        self._queries = {}
+        self.collect_queries()
+
+    def collect_queries(self):
+        sqlfiles = chain(self.sqldir.glob('*.sql'),
+                         self.sqldir.glob('*.msql'))
+
+        for sqlfile in sqlfiles:
+            filename = Path(sqlfile.name)
+            attr = filename.stem
+
+            if hasattr(self, attr):
+                # We have real namespace method which shadows
+                # this file
+                attr = f'_{attr}'
+
+            with open(sqlfile, 'r') as f:
+                self._queries[attr] = f.read()
+
+    def __getattr__(self, attr):
+        if self.cache:
+            if attr not in self._queries:
+                raise AttributeError()
+            return self._queries[attr]
+
+        if attr.startswith('_'):
+            # unshadow - see collect_queries
+            attr = attr[1:]
+
+        sqlfile = self.sqldir / f'{attr}.sql'
+        if not sqlfile.is_file():
+            sqlfile = self.sqldir / f'{attr}.msql'
+        with open(sqlfile, 'r') as f:
+            return f.read()
 
 
 class Database(type):
@@ -16,18 +60,18 @@ class db(object, metaclass=Database):
         self.carrier = carrier
 
     @classmethod
-    def register_namespace(cls, script_dir):
-        for path in Path(script_dir).iterdir():
+    def register_namespace(cls, sqldir):
+        for path in Path(sqldir).iterdir():
             if path.is_dir():
-                cls.ns[path.name] = [p for p in path.glob('*.sql')]
+                cls.ns[path.name] = Namespace(cls, path)
 
     @classmethod
-    def init(cls, connection, script_dirs, cache=True, show=False,
+    def init(cls, connection, sqldirs, cache=True, show=False,
              context_callback=None):
         cls.ns = {}
         cls.connection = connection
         cls.cache = cache
         cls.show = show
         cls.context_callback = context_callback
-        for script_dir in script_dirs:
-            cls.register_namespace(script_dir)
+        for sqldir in sqldirs:
+            cls.register_namespace(sqldir)
--- quma/tests/test_db.py
+++ quma/tests/test_db.py
@@ -1,7 +1,14 @@
-from quma import db
+from quma.db import db, Namespace
 
 
 def test_init(conn, sqldirs):
-    db.db.init(conn, sqldirs)
-    assert 'addresses' in db.db.ns
-    assert 'users' in db.db.ns
+    db.init(conn, sqldirs)
+    assert 'addresses' in db.ns
+    assert 'users' in db.ns
+
+
+def test_namespace(conn, sqldirs):
+    db.init(conn, sqldirs)
+    assert type(db.addresses) is Namespace
+    assert type(db.users) is Namespace
+    assert db.users.all.startswith('SELECT * FROM users')
