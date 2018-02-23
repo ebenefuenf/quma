ea253e9dcfeb33d94aeef15ede8cf8488878cf7e
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2018-02-24 00:42:14 +0100

    Add getconn and putconn to Pool

--- quma/pool.py
+++ quma/pool.py
@@ -1,9 +1,11 @@
 from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import NamedTupleCursor
 
 
 class Pool(object):
     def __init__(self, database, user=None, password=None, host='localhost',
-                 port='5432', minconn=1, maxconn=10):
+                 port='5432', minconn=1, maxconn=10, factory=NamedTupleCursor):
+        self.factory = factory
         self._pool = ThreadedConnectionPool(minconn,
                                             maxconn,
                                             database=database,
@@ -11,6 +13,12 @@ class Pool(object):
                                             password=password,
                                             host=host,
                                             port=port)
+
+    def getconn(self):
+        return self._pool.getconn()
+
+    def putconn(self, conn):
+        return self._pool.putconn(conn)
 
     def disconnect(self):
         self._pool.closeall()
