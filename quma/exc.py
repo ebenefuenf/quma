30f6bc8b3afb685bf93e5e81bd3734107cffe668
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2018-09-15 19:05:12 +0200

    Remove IntegrityError

--- quma/exc.py
+++ quma/exc.py
@@ -13,21 +13,3 @@ class DoesNotExistError(Exception):
 
 class OperationalError(Exception):
     pass
-
-
-class IntegrityError(Exception):
-
-    def __init__(self, *args, ref_entity=None, ref_id=None,
-                 ref_label=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ref_entity = ref_entity
-        self.ref_id = ref_id
-        self.ref_label = ref_label
-
-    def as_json(self):
-        return {
-            'errors': str(self),
-            'ref_entity': self.ref_entity,
-            'ref_id': self.ref_id,
-            'ref_label': self.ref_label
-        }
